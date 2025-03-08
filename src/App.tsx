a714154ad34b56982f79d00a7cc3bd080f3a46e8
Anshuman Singh <anshuman.singh@healthplix.com>
2025-03-08 14:06:41 +0530

    typescript rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState<any>();
+
+  console.log(count);
+  useEffect(() => {
+    console.log(count);
+  }, []);
 
   return (
-    <div className='ans-flex ans-bg-highlight ans-text-Pink-500'>
+    <div className="ans-flex ans-bg-highlight ans-text-Pink-500">
       Let's get started
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import checker from "vite-plugin-checker";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    checker({
+      typescript: {
+        tsconfigPath: "./tsconfig.app.json", // Ensure correct tsconfig is used
+        // overlay: true,
+      },
+    }),
+  ],
+});
