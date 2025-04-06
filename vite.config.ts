39c39cd17d8dca3982882cb04c9c99e57b440852
Anshuman Singh <anshuman.singh@healthplix.com>
2025-04-06 20:58:39 +0530

    vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "my-portfolio", // Your repo name
-  plugins: [react()],
+  base: "/my-portfolio/",
+  plugins: [react(), tsconfigPaths()],
 });
